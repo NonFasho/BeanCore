e9ca8e15e37c661ebdfc7726773bdb115d1da859
Sam Fawks <samfawk@limabean.xyz>
2025-04-18 02:10:54 -0400

    ^^ last commit cont.

---
+++ bean-core/src/main/java/com/bean_core/NodeConfig/ConfigLoader.java
@@ -0,0 +1,35 @@
+package com.bean_core.NodeConfig;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigLoader {
+    public static String privateKeyPath;
+    public static String bindAddress;
+    public static int networkPort;
+    public static boolean isBootstrapNode;
+    public static String bootstrapIp;
+    public static boolean isPublicNode;
+
+    public static void loadConfig() {
+        Properties props = new Properties();
+        try (FileInputStream fis = new FileInputStream("config.docs/beanchain.config.properties")) {
+            props.load(fis);
+
+            privateKeyPath = props.getProperty("privateKeyPath", "config.docs/wizkey.txt");
+            bindAddress = props.getProperty("bindAddress", "0.0.0.0");
+            networkPort = Integer.parseInt(props.getProperty("networkPort", "6442"));
+            isBootstrapNode = Boolean.parseBoolean(props.getProperty("isBootstrapNode", "false"));
+            isPublicNode = Boolean.parseBoolean(props.getProperty("isPublicNode", "false"));
+            bootstrapIp = props.getProperty("bootstrapIp", "65.38.97.169");
+
+
+        } catch (IOException e) {
+            System.err.println("⚠️ Failed to load BeanChain config: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
+
+
---
+++ bean-core/src/main/java/com/bean_core/head.java
@@ -0,0 +1,7 @@
+package com.bean_core;
+
+//just for persistent variables in versioning 
+public class head {
+    public static String version = "BETA";
+    
+}
