11bb8704756838a177b7ee6ce15fe1bc08ccdedf
Sam Fawks <samfawk@limabean.xyz>
2025-05-05 19:01:57 -0400

    Merging my new changes to the main branch  (#6)
    * Update README.md
    
    added TX class descriptions and unblocked newly added features. plus fixed the repo directions to point to the org package location
    
    * Update README.md (#2) (#3)
    
    added TX class descriptions and unblocked newly added features. plus fixed the repo directions to point to the org package location
    
    * -removed the in mem contract registry
    -added FundedCallTX (part of getting the system fully ready to handle CEN contracts)
    - changed the version in head to reflect going from pure BETA to BEANCORE 0.2.4
    - upgraded version to 0.2.4 for new package deployment
    
    * updated pom to reflect the package ownership change to BeanChain-Core
    
    * Remove target directory from Git tracking

--- bean-core/src/main/java/com/bean_core/head.java
+++ bean-core/src/main/java/com/bean_core/head.java
@@ -2,6 +2,6 @@ package com.bean_core;
 
 //just for persistent variables in versioning 
 public class head {
-    public static String version = "BETA";
+    public static String version = "BEANCORE 0.2.4";
     
 }
